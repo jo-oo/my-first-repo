f99b9b78d13415d0831ccd0f54cfb96a775f2506
Johanna Bäckström <Johanna@Johannas-MacBook-Pro-2.local>
2021-11-12 14:26:15 +0100

    Just a blankspace

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //This is my main JavaScript file
 //U = untracked. ingen spårning av vad som händer med filerna sker
 //Lägga till filer för att bli versionshanterade = git add index.html. eller tryck plus i sidofältet
-//A=Added
+//A = Added
